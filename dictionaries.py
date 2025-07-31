ecc1f93fb04f0fa0062f38c534517bb7d50f407e
Neusa Neves <neusanneves13@gmail.com>
2025-07-31 19:25:11 -0300

    Aprendendo a usar os dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+# Take o dicionario houses e olhe por Hermione na houses dicionario e ele vai ser igual a esse valor Gryffindor
+houses["Hermione"] = "Gryffindor"
+
+print(houses["Hermione"])
