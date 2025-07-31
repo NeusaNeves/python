95b9d0964f1e0e4eb6fcd105bfea2d1cea625451
Neusa Neves <neusanneves13@gmail.com>
2025-07-31 19:07:10 -0300

    Criado o loops, aprendendo que podemos realizar o loop em qualquer tipo de lista.

---
+++ loops.py
@@ -0,0 +1,22 @@
+# for i in [0, 1, 2, 3, 4, 5]:
+#     print(i)
+
+# Modo mais simples de escrever
+
+for i in range(6):
+    print(i)
+
+# Podemos realizar o loop em qualquer tipo de lista
+
+names = ["Harry", "Ron", "Hermione"]
+
+for name in names:
+    print(name)
+
+
+# Podemos ter apenas um um nome
+
+name = "Harry"
+
+for character in name:
+    print(character)
