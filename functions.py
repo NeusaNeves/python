6080e2539e7fafd590ec43b815d793347766a3e6
Neusa Neves <neusanneves13@gmail.com>
2025-07-31 21:29:14 -0300

    Aprendendo a realizar importação

--- functions.py
+++ functions.py
@@ -1,6 +1,2 @@
 def square(x):
     return x * x
-
-# Faz um loop de 0 até 9
-for i in range(10):
-    print(f"The square of {i} is {square(i)}")    
---
+++ squares.py
@@ -0,0 +1,9 @@
+# Realizando a importação
+# from functions import square
+
+# Outro modo de realizar a importação
+import functions
+
+# Faz um loop de 0 até 9
+for i in range(10):
+    print(f"The square of {i} is {functions.square(i)}") 
